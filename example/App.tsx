f0d2880a8a1440e679c864c0c5ae4b7037f44cab
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2024-06-03 09:48:25 +0200

    Add more views to the example

--- example/App.tsx
+++ example/App.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
-import { Admin, ListGuesser, Resource } from 'react-admin';
+import {
+    Admin,
+    Create,
+    EditGuesser,
+    ListGuesser,
+    Resource,
+    ShowGuesser,
+} from 'react-admin';
 import { dataProvider } from './dataProvider';
 
 export const App = () => {
     return (
         <Admin dataProvider={dataProvider}>
-            <Resource name="books" list={ListGuesser} />
-            <Resource name="authors" list={ListGuesser} />
+            <Resource
+                name="books"
+                list={ListGuesser}
+                create={BookCreate}
+                edit={EditGuesser}
+                show={ShowGuesser}
+            />
+            <Resource
+                name="authors"
+                list={ListGuesser}
+                edit={EditGuesser}
+                show={ShowGuesser}
+            />
         </Admin>
     );
 };
+
+import { Edit, ReferenceInput, SimpleForm, TextInput } from 'react-admin';
+
+export const BookCreate = () => (
+    <Create>
+        <SimpleForm>
+            <ReferenceInput source="author_id" reference="authors" />
+            <TextInput source="title" />
+        </SimpleForm>
+    </Create>
+);
